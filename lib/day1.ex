b0c54078d200b8d55f99ef0f0bdabc0fa36990ba
Joel Cartier <joel@rumandcode.io>
2022-12-04 12:33:53 -0500

    main - Day 1 - refactoring

--- lib/day1.ex
+++ lib/day1.ex
@@ -2,19 +2,23 @@ defmodule AOC2022.Day1 do
   import AOC2022.InputReader
 
   def elf_carrying_most_calories do
+    elves_calories
+    |> Enum.map(&Enum.sum/1)
+    |> Enum.reduce(fn elf_total_calories, highest_calories ->
+      max(elf_total_calories, highest_calories)
+    end)
+
+  end
+
+  def elves_calories do
     input("day1")
     |> String.split("\n\n")
-    |> Enum.reduce(0, fn elf_calories, highest_calories ->
-      elf_calories
+    |> Enum.map(fn calories ->
+      calories
       |> String.split("\n")
       |> Enum.reject(&Kernel.==(&1, ""))
-      |> Enum.map(fn calorie_str ->
-        IO.inspect(calorie_str)
-        {calorie, _} = Integer.parse(calorie_str)
-        calorie
-      end)
-      |> Enum.sum()
-      |> Kernel.max(highest_calories)
+      |> Enum.map(&Integer.parse/1)
+      |> Enum.map(&Kernel.elem(&1, 0))
     end)
   end
 end
