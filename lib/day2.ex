79779797c1ae5132f92bf40ce876a54706f2be7e
Joel Cartier <joel@rumandcode.io>
2022-12-04 21:21:41 -0500

    main - Day 2 - answer 2

--- lib/day2.ex
+++ lib/day2.ex
@@ -1,40 +1,46 @@
 defmodule AOC2022.Day2 do
   import AOC2022.InputReader
 
-  def calculate_total_score() do
-    input("day2")
-    |> String.split("\n")
-    |> Enum.reject(&Kernel.==(&1, ""))
-    |> Enum.map(fn round_str ->
-      round_str
-      |> String.split(" ")
-      |> Enum.slice(0..1)
-      |> List.to_tuple()
-      |> calculate_round_points()
-    end)
+  @interpretations [
+    :shape_vs_shape,
+    :shape_vs_outcome
+  ]
+
+  def calculate_total_score(interpretation) do
+    parsed_input()
+    |> Enum.map(&calculate_round_points(&1, interpretation))
     |> Enum.sum()
   end
 
-  def calculate_round_points({opponent_choice_char, your_choice_char}) do
-    calculate_round_points(opponent_choice_char, your_choice_char)
+  def calculate_round_points({column_a, column_b}, :shape_vs_shape = strategy) do
+    opponent_shape = map_column_a(column_a)
+    your_shape = map_column_b(column_b, strategy)
+
+    outcome = interaction_outcome(opponent_shape, your_shape)
+
+    calculate_round_points(your_shape, outcome)
   end
 
-  def calculate_round_points(opponent_choice_char, your_choice_char) do
-    opponent_shape = map_opponent_shape(opponent_choice_char)
-    your_shape = map_your_shape(your_choice_char)
+  def calculate_round_points({column_a, column_b}, :shape_vs_outcome = strategy) do
+    opponent_shape = map_column_a(column_a)
+    outcome = map_column_b(column_b, strategy)
 
-    interaction_outcome(opponent_shape, your_shape)
-    |> points_for_outcome()
-    |> Kernel.+(points_for_drawn_choice(your_shape))
+    your_shape = your_shape_for_interaction(opponent_shape, outcome)
+
+    calculate_round_points(your_shape, outcome)
   end
 
-  @drawn_choice_points %{
+  def calculate_round_points(your_shape, outcome) do
+    points_for_outcome(outcome) + points_for_drawn_shape(your_shape)
+  end
+
+  @drawn_shape_points %{
     rock: 1,
     paper: 2,
     scissors: 3
   }
-  def points_for_drawn_choice(choice) do
-    Map.get(@drawn_choice_points, choice)
+  def points_for_drawn_shape(shape) do
+    Map.get(@drawn_shape_points, shape)
   end
 
   @outcome_points %{
@@ -52,17 +58,33 @@ defmodule AOC2022.Day2 do
     scissors: 2
   }
   def interaction_outcome(opponent_shape, your_shape) do
-    opponent_choice_power = Map.get(@shape_numerical_power, opponent_shape)
-    your_choice_power = Map.get(@shape_numerical_power, your_shape)
+    opponent_shape_power = Map.get(@shape_numerical_power, opponent_shape)
+    your_shape_power = Map.get(@shape_numerical_power, your_shape)
 
     cond do
-      opponent_choice_power == your_choice_power ->
+      opponent_shape_power == your_shape_power ->
         :draw
-      opponent_choice_power == rem(your_choice_power + 1, 3) ->
+      opponent_shape_power == rem(your_shape_power + 1, 3) ->
         :lose
       true ->
         :win
     end
+  end
+
+  def your_shape_for_interaction(opponent_shape, outcome) do
+    opponent_shape_power = Map.get(@shape_numerical_power, opponent_shape)
+    numerical_power_shape = for {shape, power} <- @shape_numerical_power, into: %{}, do: {power, shape}
+
+    your_shape_power = case outcome do
+      :draw ->
+        opponent_shape_power
+      :win ->
+        rem(opponent_shape_power + 1, 3)
+      :lose ->
+        rem(opponent_shape_power + 2, 3)
+    end
+
+    Map.get(numerical_power_shape, your_shape_power)
   end
 
   @opponent_shapes %{
@@ -70,7 +92,7 @@ defmodule AOC2022.Day2 do
     "B" => :paper,
     "C" => :scissors
   }
-  def map_opponent_shape(opponent_char) do
+  def map_column_a(opponent_char) do
     case Map.get(@opponent_shapes, opponent_char) do
       nil ->
         raise "unexpected char #{opponent_char} for opponent's drawn char"
@@ -84,13 +106,39 @@ defmodule AOC2022.Day2 do
     "Y" => :paper,
     "Z" => :scissors
   }
-  def map_your_shape(your_char) do
+  def map_column_b(your_char, :shape_vs_shape) do
     case Map.get(@your_shapes, your_char) do
       nil ->
         raise "unexpected char #{your_char} for your drawn char"
       shape ->
         shape
     end
+  end
+
+  @your_outcomes %{
+    "X" => :lose,
+    "Y" => :draw,
+    "Z" => :win
+  }
+  def map_column_b(outcome_char, :shape_vs_outcome) do
+    case Map.get(@your_outcomes, outcome_char) do
+      nil ->
+        raise "unexpected char #{outcome_char} for your outcome char"
+      shape ->
+        shape
+    end
+  end
+
+  def parsed_input() do
+    input("day2")
+    |> String.split("\n")
+    |> Enum.reject(&Kernel.==(&1, ""))
+    |> Enum.map(fn round_str ->
+      round_str
+      |> String.split(" ")
+      |> Enum.slice(0..1)
+      |> List.to_tuple()
+    end)
   end
 
 end
